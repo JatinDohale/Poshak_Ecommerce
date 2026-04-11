386f6d552acb62d8f1a5875ee74576df8af73f50
Jatin Dohale <jatindohale2004@gmail.com>
2026-04-11 19:16:12 +0530

    Add console log for decoded JWT token
    Log decoded JWT token information for debugging.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,6 +7,7 @@ const authUser = async (req, res, next) => {
 
    try {
       const token_decode = jwt.verify(token, process.env.SECRET_KEY)
+      console.log('decoded:', token_decode);
       req.body.userID = token_decode.id;
       next()
 
